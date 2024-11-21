ee56524820b4b9ff6722c4d773a62390dabd742c
AndreasKy Kyprianou <you@example.com>
2024-11-21 12:06:24 +0200

    Εισαγωγή κώδικα

--- Maintest.java
+++ Maintest.java
@@ -1,5 +1,6 @@
 public class Maintest {
     public static void main(String[] args) {
         System.out.println("first line of code");
+        System.out.println("Second line of the code");
     }
 }
