88ae795f09e1ad7502b5287003c048b2a3463e37
Odysseas Kerasaridis <okerasar@gmail.com>
2024-11-22 23:40:30 +0200

    Created maven modules: interface, optimization, data retrieval

---
+++ interface/src/main/java/com/athensease/App.java
@@ -0,0 +1,13 @@
+package com.athensease;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
