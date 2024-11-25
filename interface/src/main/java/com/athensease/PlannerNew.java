eef28ef5eb3337c2e9fb735c48efa8a4ebd96ff6
Lydia Chari <t8230159@aueb.gr>
2024-11-25 15:48:50 +0000

    added PlannerNew and Questionnaire

---
+++ interface/src/main/java/com/athensease/PlannerNew.java
@@ -0,0 +1,10 @@
+public class PlannerNew {
+    
+    public static void main (String args[]) {
+        QuestionnaireNew questionnaire = new QuestionnaireNew();
+        questionnaire.gatherHolidayDuration();
+        questionnaire.gatherTrailheads();
+        questionnaire.gatherBudget();
+        questionnaire.chooseCategories();
+    }
+}
